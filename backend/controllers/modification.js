6f015b78dd1545ff80aa3c039706a30a2c59e570
AgatheSk <ehtaga.kiratus@gmail.com>
2022-05-03 16:43:20 +0200

    ajout modif email +correction pour mdp

--- backend/controllers/modification.js
+++ backend/controllers/modification.js
@@ -60,22 +60,23 @@ const ChangementMdp = (req, res) => {
                                 console.log("Bon mot de passe de la classe")
                                 // on change le mdp
                                 // si le mot de passe entré correspond bien au mot de passe dans la base de données
-                                
+
                                 bcrypt.hash(newMdp, 10, (err, hash) => {
-                                    if(err){
+                                    if (err) {
                                         //erreur lors du hahage
                                         res.sendStatus(300)
                                     }
                                     const newclasse = Classe.update(
-                                    {
-                                        motdepasse: hash,
-                                    },
-                                    {
-                                        where: { courriel: email },
-                                    }
-                                );}
+                                        {
+                                            motdepasse: hash,
+                                        },
+                                        {
+                                            where: { courriel: email },
+                                        }
+                                    );
+                                    res.send(newclasse)
+                                });
                                 // on envoie la nouvelle classe 
-                                res.send(newclasse)
                             } else {
                                 console.log("Mauvais mot de passe classe")
                                 res.sendStatus(400)
@@ -91,7 +92,7 @@ const ChangementMail = (req, res) => {
     const email = req.body.mail;
     const newEmail = req.body.newMail;
     const mdp = req.body.pwd;
-    
+
     console.log("*** Vérification mail ***")
     if (!(mdp.match("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,24}$"))) {
         console.log("taille mdp pas ok")
@@ -108,61 +109,61 @@ const ChangementMail = (req, res) => {
         res.sendStatus(407)
     }
 
-      // on cherche un eleve qui a le mail donné
-      Eleve.findOne({ where: { courriel: email } })
-      .then(eleve => {
-          if (eleve) {
-              // on vérifie maintenant dans la bd si le mdp donné est bien celui associé au mail
-              bcrypt.compare(mdp, eleve.motdepasse, function (err, estValide) {
-                  if (estValide) {
-                      console.log("Bon mot de passe de l'élève")
-                      // on change le mdp
-                      const neweleve = Eleve.update(
-                          {
-                              courriel: newEmail,
-                          },
-                          {
-                              where: { motdepasse: mdp },
-                          }
-                      );
-                      // si le mot de passe entré correspond bien au mot de passe dans la base de données
-                      res.send(neweleve)
-                  } else {
-                      console.log("Mauvais mot de passe ELEVE")
-                      res.sendStatus(400)
-                  }
-              });
-              // si on ne trouve pas, c'est peut etre une classe
-          } else {
-              Classe.findOne({ where: { courriel: email } })
-                  .then(classe => {
-                      if (!classe) {
-                          res.status(404).send("Aucun compte correspondant à cet adresse.")
-                      }
-                      // comparaion du mdp avec celui connu
-                      bcrypt.compare(mdp, classe.motdepasse, function (err, estValide) {
-                          if (estValide) {
-                              console.log("Bon mot de passe de la classe")
-                              // on change le mdp
-                              // si le mot de passe entré correspond bien au mot de passe dans la base de données
-                              const newclasse = Classe.update(
-                                  {
-                                    courriel: newEmail,
-                                  },
-                                  {
-                                      where: {motdepasse: mdp },
-                                  }
-                              );
-                              // on envoie la nouvelle classe 
-                              res.send(newclasse)
-                          } else {
-                              console.log("Mauvais mot de passe classe")
-                              res.sendStatus(400)
-                          }
-                      });
-                  })
-          }
-      });
+    // on cherche un eleve qui a le mail donné
+    Eleve.findOne({ where: { courriel: email } })
+        .then(eleve => {
+            if (eleve) {
+                // on vérifie maintenant dans la bd si le mdp donné est bien celui associé au mail
+                bcrypt.compare(mdp, eleve.motdepasse, function (err, estValide) {
+                    if (estValide) {
+                        console.log("Bon mot de passe de l'élève")
+                        // on change le mdp
+                        const neweleve = Eleve.update(
+                            {
+                                courriel: newEmail,
+                            },
+                            {
+                                where: { motdepasse: mdp },
+                            }
+                        );
+                        // si le mot de passe entré correspond bien au mot de passe dans la base de données
+                        res.send(neweleve)
+                    } else {
+                        console.log("Mauvais mot de passe ELEVE")
+                        res.sendStatus(400)
+                    }
+                });
+                // si on ne trouve pas, c'est peut etre une classe
+            } else {
+                Classe.findOne({ where: { courriel: email } })
+                    .then(classe => {
+                        if (!classe) {
+                            res.status(404).send("Aucun compte correspondant à cet adresse.")
+                        }
+                        // comparaion du mdp avec celui connu
+                        bcrypt.compare(mdp, classe.motdepasse, function (err, estValide) {
+                            if (estValide) {
+                                console.log("Bon mot de passe de la classe")
+                                // on change le mdp
+                                // si le mot de passe entré correspond bien au mot de passe dans la base de données
+                                const newclasse = Classe.update(
+                                    {
+                                        courriel: newEmail,
+                                    },
+                                    {
+                                        where: { motdepasse: mdp },
+                                    }
+                                );
+                                // on envoie la nouvelle classe 
+                                res.send(newclasse)
+                            } else {
+                                console.log("Mauvais mot de passe classe")
+                                res.sendStatus(400)
+                            }
+                        });
+                    })
+            }
+        });
 
     res.sendStatus(404)
 }
