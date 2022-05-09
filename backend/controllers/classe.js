e1a42ddc87c3885413d7ab34e370760a9142f46a
Agathe Sutarik <74046840+AgatheSk@users.noreply.github.com>
2022-05-09 11:26:53 +0200

    Changement classe pour renvoyer le mail

--- backend/controllers/classe.js
+++ backend/controllers/classe.js
@@ -2,23 +2,26 @@ const Class = require('../models/users').Classe
 const Eleve = require('../models/users').Eleve
 
 const getAllStudents = (req, res) =>{
-    const mail = req.query.mail
+const mail = req.query.mail
     console.log("yo")
-    Class.findAll({
-        where :{
-            courriel:mail
-        },
+    Eleve.findAll({
+        attributes:['courriel'],
+       
         // jointure avec la table eleve
         include: [{
-            model :Eleve,
-            required: true
+            model :Classe,
+            attributes:[],
+           required: true,
+            where :{
+                courriel:mail
+            }
         }]
     }).then(eleves =>{
         for(e in eleves){
-            console.log('élève '+e.courriel)
+            console.log('élève '+e)
         }
        // res.json({students:eleves})
-       res.json({mail:eleves.courriel})
+       res.json({eleves:eleves})
     })
 }
 
