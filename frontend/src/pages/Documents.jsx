93ac6f53eb8364eb3cffedbd51331d7d1f5e7bd6
Romain Reghem <romain.reghem@etud.univ-jfc.fr>
2022-05-17 16:56:09 +0200

    refresh button for files

--- frontend/src/pages/Documents.jsx
+++ frontend/src/pages/Documents.jsx
@@ -1,6 +1,9 @@
 import PDFSender from "../components/PDFSender"
 import FileList from "../components/FileList"
 import PDFViewer from "../components/PDFViewer";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faRotate } from "@fortawesome/free-solid-svg-icons";
+
 import axios from "../api/axios";
 import useAuth from "../hooks/useAuth";
 import { useState, useEffect } from "react";
@@ -15,12 +18,15 @@ const Documents = () => {
     // Le fichier actuellement séléctionné dans la liste. On le passera à PDFViewer.
     const [file, setFile] = useState(null);
 
+    const [loadingFiles, setLoadingFiles] = useState(false);
+
     useEffect(() => {
         loadFiles()
     }, [])
 
     const loadFiles = async () => {
-        try {            
+        try {         
+            setLoadingFiles(true);   
             const response = await axios.get("/getCours", {
                 params: { mail: auth?.user, cours: "maths" },
                 headers: { 'Content-Type': 'application/json' },
@@ -29,12 +35,15 @@ const Documents = () => {
             setFiles(response.data.files)            
         } catch (error) {
             console.log(error)
+            
         }
+        setLoadingFiles(false);
     }
 
     return (
         <div className="fileMain">
             <section className="fileManager">
+                <div onClick={loadFiles} style={{alignSelf:"flex-start", marginBottom:"0.5rem"}}><FontAwesomeIcon className="fileRefresh" icon={faRotate} spin={loadingFiles}/></div>
                 <div className="fileList">
                     {Array.from(Array(files?.length), (e, i) => {
                         return <div key={i} onClick={() => setFile(files[i])} className="file">{files[i]}</div>
