2d7cd615fcc3703f704f2e29e2600e910008d9a6
AgatheSk <ehtaga.kiratus@gmail.com>
2022-05-25 11:35:40 +0200

    changement pour les refresh tokens : stockage dans la bd

--- backend/controllers/refreshToken.js
+++ backend/controllers/refreshToken.js
@@ -1,5 +1,3 @@
-const connexion = require('./connexion')
-let refreshTokens = connexion.refreshTokens;
 const jwt = require('jsonwebtoken');
 const { getInvitation } = require('./eleve');
 const Users = require('../models/users');
@@ -24,73 +22,72 @@ const refreshToken = (req, res) => {
         return res.status(401).send("accès refusé")
     }
     const refreshToken = cookies.jwt;
-    /*if (!refreshTokens.includes(refreshToken)) {
-        //token invalide 
-        return res.status(403).send("accès interdit")
-    }*/
-    Refresh.findOne({attributes:['idtoken'], where:{token:refreshToken}})
-    .then(token=>{
-        if(!token){
-            console.log("le token n'a pas été trouvé")
-            return res.status(403).send("accès interdit")
-        }
-        console.log('REFRESH ' + refreshToken)
-        jwt.verify(
-            refreshToken,
-            process.env.REFRESH_TOKEN_SECRET,
-            (err, decoded) => {
-                if (err || decoded == undefined) {
-                    console.log("probleme lors de la verification "+err);
-                    // accès interdit
-                    return res.sendStatus(403);
-                } else {
-                    console.log("decoded " + decoded.mail + " : " + decoded.role)
-                    const accessToken = jwt.sign(
-                        {
-                            "UserInfo": {
-                                "mail": decoded.mail,
-                                "role": decoded.role
-                            }
-                        },
-                        process.env.ACCESS_TOKEN_SECRET,
-                        { expiresIn: '10m' });
-                    // si c'est un élève
-                    if (decoded.role == "eleve") {
-                        // on doit récupèrer l'état de l'invitation pour le transmettre au serveur
-                        getInvitation(decoded.mail, function (reponse) {
-                            if (reponse == 404 || reponse == 407) {
-                                console.log("Erreur lors de la récupération de l'invitation "+reponse)
-                                return res.sendStatus(reponse)
-                            } else {
-                                console.log('envoi des infos')
-                                return res.status(201).json(Object.assign({ role: decoded.role, accessToken: accessToken }, reponse));
-                            }
+
+    Refresh.findOne({ attributes: ['idtoken'], where: { token: refreshToken } })
+        .then(token => {
+            if (!token) {
+                console.log("le token n'a pas été trouvé")
+                return res.status(403).send("accès interdit")
+            }
+            console.log('REFRESH ' + refreshToken)
+            jwt.verify(
+                refreshToken,
+                process.env.REFRESH_TOKEN_SECRET,
+                (err, decoded) => {
+                    if (err || decoded == undefined) {
+                        console.log("probleme lors de la verification " + err);
+                        Refresh.destroy({ where: { idtoken: token.idtoken } }).then(() => {
+                            // accès interdit
+                            return res.sendStatus(403);
                         })
                     } else {
-                        Classe.findOne({ attributes: ['idclasse'], where: { courriel: decoded.mail } })
-                            .then(classe => {
-                                if (!classe) {
-                                    console.log("pas de classe avec le mail : "+decoded.mail)
-                                    // le mail ne correspond à aucune mail : accès interdit
-                                    return res.sendStatus(403)
+                        console.log("decoded " + decoded.mail + " : " + decoded.role)
+                        const accessToken = jwt.sign(
+                            {
+                                "UserInfo": {
+                                    "mail": decoded.mail,
+                                    "role": decoded.role
                                 }
-                                // sinon si c'est une classe on retourne juste le role et le nouveau accesstoken + l'id de la classe
-                                return res.status(201).json({ role: decoded.role, accessToken: accessToken, idclasse: classe.idclasse });
-                            }
-                            ).catch(err => {
-                                console.log("erreur lors de la recup de classe "+err)
-                                return res.send(err).status(520)
-                            });
-    
+                            },
+                            process.env.ACCESS_TOKEN_SECRET,
+                            { expiresIn: '10m' });
+                        // si c'est un élève
+                        if (decoded.role == "eleve") {
+                            // on doit récupèrer l'état de l'invitation pour le transmettre au serveur
+                            getInvitation(decoded.mail, function (reponse) {
+                                if (reponse == 404 || reponse == 407) {
+                                    console.log("Erreur lors de la récupération de l'invitation " + reponse)
+                                    return res.sendStatus(reponse)
+                                } else {
+                                    console.log('envoi des infos')
+                                    return res.status(201).json(Object.assign({ role: decoded.role, accessToken: accessToken }, reponse));
+                                }
+                            })
+                        } else {
+                            Classe.findOne({ attributes: ['idclasse'], where: { courriel: decoded.mail } })
+                                .then(classe => {
+                                    if (!classe) {
+                                        console.log("pas de classe avec le mail : " + decoded.mail)
+                                        // le mail ne correspond à aucune mail : accès interdit
+                                        return res.sendStatus(403)
+                                    }
+                                    // sinon si c'est une classe on retourne juste le role et le nouveau accesstoken + l'id de la classe
+                                    return res.status(201).json({ role: decoded.role, accessToken: accessToken, idclasse: classe.idclasse });
+                                }
+                                ).catch(err => {
+                                    console.log("erreur lors de la recup de classe " + err)
+                                    return res.send(err).status(520)
+                                });
+
+                        }
+
                     }
-    
                 }
-            }
-        )
-    }).catch(err => {
-        console.log("Erreur lors de la récup des tokens : "+err)
-        return res.send(err).status(520)
-    });
+            )
+        }).catch(err => {
+            console.log("Erreur lors de la récup des tokens : " + err)
+            return res.send(err).status(520)
+        });
 }
 
 module.exports = { refreshToken };
