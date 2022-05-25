81b851dab4bdf95de76abcdecee57b21377b6c89
Romain Reghem <romain.reghem@etud.univ-jfc.fr>
2022-05-25 12:17:50 +0200

    tictactoe v1

--- backend/tictactoe.js
+++ backend/tictactoe.js
@@ -11,38 +11,89 @@ io.on("connection", (socket) => {
     const numClients = clients ? clients.size : 0;
 
     // Il n'y a personne, on ajoute donc le premier joueur à la salle
-    if(numClients == 0) {
-      socket.join(roomCode); 
+    if (numClients == 0) {
+      socket.join(roomCode);
       socket.emit("YoureIn");
       // On donne un pseudo au socket 
       socket.nickname = "player1";
-    } 
+      rooms.get(roomCode).player1 = socket.id;
+    }
 
     // Il y a déjà quelqu'un dans la salle, on ajoute un deuxième joueur...
-    else if(numClients == 1){
-      socket.join(roomCode); 
+    else if (numClients == 1) {
+      socket.join(roomCode);
       socket.emit("YoureIn");
       // On donne un pseudo au socket 
       socket.nickname = "player2";
+      rooms.get(roomCode).player2 = socket.id;
 
       // ... et on commence la partie
       rooms.get(roomCode).board = ["", "", "", "", "", "", "", "", ""]
-      io.in(roomCode).emit("maxPlayersReached", rooms.get(roomCode).board);
-    } 
+      io.in(roomCode).emit("maxPlayersReached", rooms.get(roomCode).board, rooms.get(roomCode).player1);
+    }
 
     else {
       socket.emit("gameIsFull");
     }
     console.log(`A user joined the room ${roomCode}, there are now ${numClients} clients in the room.`);
-    
+
   });
 
   socket.on("play", ({ id, roomCode }) => {
-    console.log(`play at ${id} to ${roomCode}`);
-    socket.broadcast.to(roomCode).emit("updateGame", id);
+    // First player plays X
+    socket.nickname == "player1"
+      ? rooms.get(roomCode).board[id] = "X"
+      // Second player plays O
+      : rooms.get(roomCode).board[id] = "O";
+
+    socket.broadcast.to(roomCode).emit("ennemyPlayed");
+    io.in(roomCode).emit("updateGame", rooms.get(roomCode).board);
+    checkVictory(roomCode);
   });
 
+  checkVictory = (roomCode) => {
+    let board = rooms.get(roomCode).board;
+
+    // Lines victory conditions
+    for (let i = 0; i <= 6; i += 3) {
+      console.log(i)
+      if (board[i] == "X" && board[i + 1] == "X" && board[i + 2] == "X") {
+        io.in(roomCode).emit("victory", rooms.get(roomCode).player1);
+        io.in(roomCode).socketsLeave(roomCode);
+
+      } else if (board[i] == "O" && board[i + 1] == "O" && board[i + 2] == "O") {
+        io.in(roomCode).emit("victory", rooms.get(roomCode).player2);
+        io.in(roomCode).socketsLeave(roomCode);
+
+      }
+    }
+
+    // Column victory conditions
+    for (let i = 0; i <= 3; i += 1) {
+      if (board[i] == "X" && board[i + 3] == "X" && board[i + 6] == "X") {
+        io.in(roomCode).emit("victory", rooms.get(roomCode).player1);
+        io.in(roomCode).socketsLeave(roomCode);
+
+      } else if (board[i] == "O" && board[i + 3] == "O" && board[i + 6] == "O") {
+        io.in(roomCode).emit("victory", rooms.get(roomCode).player2);
+        io.in(roomCode).socketsLeave(roomCode);
+
+      }
+    }
+
+    // Diagonals victory conditions
+    if ((board[0] == "X" && board[4] == "X" && board[8] == "X") || (board[2] == "X" && board[4] == "X" && board[6] == "X")) {
+      io.in(roomCode).emit("victory", rooms.get(roomCode).player1);
+      io.in(roomCode).socketsLeave(roomCode);
+
+    } else if ((board[0] == "O" && board[4] == "O" && board[8] == "O") || (board[2] == "O" && board[4] == "O" && board[6] == "O")) {
+      io.in(roomCode).emit("victory", rooms.get(roomCode).player2);
+      io.in(roomCode).socketsLeave(roomCode);
+    }
+  }
+
   socket.on("disconnect", () => {
+    //io.in(roomCode).emit("disconnect");
     console.log("User Disconnected");
   });
 });
--- frontend/src/pages/TicTacToe.jsx
+++ frontend/src/pages/TicTacToe.jsx
@@ -1,16 +1,36 @@
 import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import * as React from 'react';
 import Cell from "../components/Cell";
 import "../styles/tictactoe.css";
 import CircularProgress from '@mui/material/CircularProgress';
 import io from "socket.io-client"
+import Button from "@mui/material/Button";
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogTitle from '@mui/material/DialogTitle';
+import Slide from '@mui/material/Slide';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faXmark } from "@fortawesome/free-solid-svg-icons";
+import { faCircle } from "@fortawesome/free-regular-svg-icons"
 
 const socket = io.connect("http://localhost:5000")
 
+const Transition = React.forwardRef(function Transition(props, ref) {
+    return <Slide direction="up" ref={ref} {...props} />;
+});
+
 const TicTacToe = () => {
     const [roomCode, setRoomCode] = useState("53");
-    const [roomFull, setRoomFull] = useState(false);
     const [roomJoined, setRoomJoined] = useState(false);
     const [waitingText, setWaitingText] = useState("");
+    const [gameEnded, setGameEnded] = useState(false);
+    const [showBoard, setShowBoard] = useState(false);
+    const [win, setWin] = useState(false)
+    const [board, setBoard] = useState(["", "", "", "", "", "", "", "", ""]);
+    const [UIboard, setUIboard] = useState([])
+    const [canPlay, setCanPlay] = useState(true);
+
     useEffect(() => {
         console.log(roomCode);
         if (roomCode && !roomJoined) {
@@ -18,22 +38,13 @@ const TicTacToe = () => {
         }
     }, [roomCode]);
 
-    const [board, setBoard] = useState(["", "", "", "", "", "", "", "", ""]);
-    const [canPlay, setCanPlay] = useState(true);
-
     useEffect(() => {
-        socket.on("updateGame", (id) => {
-            console.log("use Effect", id);
-            setBoard((data) => ({ ...data, [id]: "O" }));
-            setCanPlay(true);
-        });
-        return () => socket.off("updateGame");
-    });
-
-    useEffect(() => {
-        socket.on("maxPlayersReached", (b) => {
-            setRoomFull(true);
-            console.log(b)
+        socket.on("maxPlayersReached", (b, player1) => {
+            setBoard(b);
+            setUIboard([])
+            setShowBoard(true);
+            // Si ce socket est le premier joueur, il peut jouer en premier
+            socket.id == player1 ? setCanPlay(true) : setCanPlay(false);
         });
 
         socket.on("YoureIn", () => {
@@ -45,55 +56,97 @@ const TicTacToe = () => {
             console.log("Sorry, game is full");
             setWaitingText("Deux joueurs sont déjà en partie !")
         });
+
+        socket.on("updateGame", (updatedBoard) => {
+            setBoard(updatedBoard);
+
+            // Making a new array to have nicer UI than just letters "X" and "O"
+            let newUIboard = [];
+            updatedBoard.forEach((cell) => {
+                cell == "X" ? newUIboard.push(<FontAwesomeIcon icon={faXmark}/>)
+                : cell == "O" ? newUIboard.push(<FontAwesomeIcon icon={faCircle}/>)
+                : newUIboard.push("")
+            })
+            setUIboard(newUIboard)
+
+        });
+
+        socket.on("ennemyPlayed", () => {
+            setCanPlay(true);
+        });
+
+        socket.on("victory", (player) => {
+            socket.id == player ? setWin(true) : setWin(false)
+            setGameEnded(true);
+            setRoomJoined(false);
+
+        });
+
+        socket.on("disconnect", () => {
+            console.log("Quelqu'un a quitté la partie")
+        })
     })
-
-    useEffect(() => {
-        for (let i = 0; i <= 3; i += 3) {
-            if (board[i] == "X" && board[i + 1] == "X" && board[i + 2] == "X") {
-                console.log("gagné ligne !")
-            }
-        }
-
-    }, [board])
 
     const handleCellClick = (e) => {
         const id = e.currentTarget.id;
         if (canPlay && board[id] == "") {
-            setBoard((data) => ({ ...data, [id]: "X" }));
             socket.emit("play", { id, roomCode });
             setCanPlay(false);
         }
-
-        if (
-            (board[0] == "X" && board[1] == "X" && board[2] == "X") ||
-            (board[0] == "O" && board[1] == "O" && board[2] == "O")
-        ) {
-            setBoard(["", "", "", "", "", "", "", "", ""]);
-        }
     };
+
+    const handleReplay = () => {
+        // On remet la partie à 0
+        setGameEnded(false);
+        setShowBoard(false);
+        setWin(false);
+        socket.emit("joinRoom", roomCode);
+    }
 
     return (
         <>
-            {roomFull && roomJoined ? (
+            {showBoard ? (
                 <div className="tictactoe">
                     <section className="tictactoe-section">
-                        <Cell handleCellClick={handleCellClick} id={"0"} text={board[0]} />
-                        <Cell handleCellClick={handleCellClick} id={"1"} text={board[1]} />
-                        <Cell handleCellClick={handleCellClick} id={"2"} text={board[2]} />
+                        <Cell handleCellClick={handleCellClick} id={"0"} text={UIboard[0]} />
+                        <Cell handleCellClick={handleCellClick} id={"1"} text={UIboard[1]} />
+                        <Cell handleCellClick={handleCellClick} id={"2"} text={UIboard[2]} />
 
-                        <Cell handleCellClick={handleCellClick} id={"3"} text={board[3]} />
-                        <Cell handleCellClick={handleCellClick} id={"4"} text={board[4]} />
-                        <Cell handleCellClick={handleCellClick} id={"5"} text={board[5]} />
+                        <Cell handleCellClick={handleCellClick} id={"3"} text={UIboard[3]} />
+                        <Cell handleCellClick={handleCellClick} id={"4"} text={UIboard[4]} />
+                        <Cell handleCellClick={handleCellClick} id={"5"} text={UIboard[5]} />
 
-                        <Cell handleCellClick={handleCellClick} id={"6"} text={board[6]} />
-                        <Cell handleCellClick={handleCellClick} id={"7"} text={board[7]} />
-                        <Cell handleCellClick={handleCellClick} id={"8"} text={board[8]} />
+                        <Cell handleCellClick={handleCellClick} id={"6"} text={UIboard[6]} />
+                        <Cell handleCellClick={handleCellClick} id={"7"} text={UIboard[7]} />
+                        <Cell handleCellClick={handleCellClick} id={"8"} text={UIboard[8]} />
                     </section>
-                </div>) :
-                <div className="waitingRoom">
+                    {canPlay ? (<Button style={{ marginTop: "0.5rem" }} variant="contained" color="success">
+                        A ton tour de jouer !
+                    </Button>) : (<Button style={{ marginTop: "0.5rem" }} variant="outlined" color="error">
+                        <CircularProgress style={{ marginRight: "0.5rem" }} size={10} color="error" />
+                        Tour de l'adversaire...
+                    </Button>)}
+
+                    {gameEnded ? <Dialog
+                        open={gameEnded}
+                        TransitionComponent={Transition}
+                        keepMounted>
+                        <center><DialogTitle>{win ? "Gagné !" : "Perdu :("}</DialogTitle></center>
+                        <DialogActions>
+                            <Button onClick={handleReplay} variant="outlined" color="success">Rejouer</Button>
+                            <Link to="/jeux" style={{textDecoration: 'none', marginLeft:"0.3rem"}} >
+                                <Button variant="outlined" color="error">Quitter</Button>
+                            </Link>
+                        </DialogActions>
+                    </Dialog> : console.log("partie en cours")}
+                </div>
+
+
+            ) :
+                (<div className="waitingRoom">
                     {roomJoined ? <CircularProgress /> : <></>}
                     <p>{waitingText}</p>
-                </div>
+                </div>)
             }
         </>
     );
